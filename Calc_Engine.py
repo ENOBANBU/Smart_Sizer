34d710405fba29ac4739d91c375c1b3acae2f1e5
Eniola Bamgbelu <gbemds66@gmail.com>
2026-06-01 00:07:10 -0500

    Added function to convert the pixels found in picture into  comparable centimeters.

--- Calc_Engine.py
+++ Calc_Engine.py
@@ -11,3 +11,5 @@ vol = disk_method(x, r)
 print(f"Got: {vol:.2f}") #should print 785.40
 print(f"Expected: {np.pi * 5**2 * 10:.2f}")
 """
+def convert_pix(pixel_cords, pixel_cm):
+    return pixel_cords/pixel_cm
