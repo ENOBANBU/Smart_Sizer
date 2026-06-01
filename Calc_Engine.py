c61258535fea6468fd60884b50bbff82885465e5
Eniola Bamgbelu <gbemds66@gmail.com>
2026-06-01 00:22:03 -0500

    created function for finding the center line of the object and the orientation "vertical" or "horizontal" tp decided the center axis and the way the object should be rotated. contour points grab mathematical boundary of object to compare witdth and height as well as decided the x and y axis.

--- Calc_Engine.py
+++ Calc_Engine.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 def disk_method(x_cords, r_cords):
-
     return np.trapz(np.pi * r_cords**2, x_cords)
 
 """
@@ -13,3 +12,17 @@ print(f"Expected: {np.pi * 5**2 * 10:.2f}")
 """
 def convert_pix(pixel_cords, pixel_cm):
     return pixel_cords/pixel_cm
+
+def find_axis(con_pts):
+    x_cords = con_pts[:, 0]
+    y_cords = con_pts[:, 1]
+
+    x_axis = (x_cords.max() + x_cords.min()) / 2
+    y_axis = (y_cords.max() + y_cords.min()) / 2
+
+    height = y_cords.max() - y_cords.min()
+    width = x_cords.max() - x_cords.min()
+
+    orientation = 'Top - Bottom' if height > width else 'left - right'
+
+    return x_axis, y_axis, orientation
