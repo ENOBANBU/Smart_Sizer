3641c09cda876eff35de12eb10af6d1a2a5aed2d
Eniola Bamgbelu <gbemds66@gmail.com>
2026-05-31 23:52:37 -0500

    added code to check disk_method function which proves to work

--- Calc_Engine.py
+++ Calc_Engine.py
@@ -3,3 +3,11 @@ import numpy as np
 def disk_method(x_cords, r_cords):
 
     return np.trapz(np.pi * r_cords**2, x_cords)
+
+"""
+x = np.linspace(0, 10, 100)
+r = np.full(100, 5.0)
+vol = disk_method(x, r)
+print(f"Got: {vol:.2f}") #should print 785.40
+print(f"Expected: {np.pi * 5**2 * 10:.2f}")
+"""
