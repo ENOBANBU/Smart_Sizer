70b5f39810a298a4c517b02bf1f5cb28d9a5ef3a
Eniola Bamgbelu <gbemds66@gmail.com>
2026-06-06 19:08:55 -0500

    load needed imports and wrote code for image loading function

--- Jarvis.py
+++ Jarvis.py
@@ -1,1 +1,11 @@
-#This is the function for the ML model
+import cv2
+import numpy as np
+from rembg import remove
+from PIL import Image
+import io
+
+def load_img(path):
+    img = cv2.imread(path)
+    if img is None:
+        raise FileNotFoundError(f"Could not load image from path: {path}")
+    return img
