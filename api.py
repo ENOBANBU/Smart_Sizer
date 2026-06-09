6a56f19686f17b9939c56f0cb4d8f59cbccb349e
Eniola Bamgbelu <gbemds66@gmail.com>
2026-06-08 20:27:50 -0500

    figuring out how to use fast api

--- api.py
+++ api.py
@@ -1,1 +1,8 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
+
+app = FastAPI() #creates HTTP
+
+app.get("/") #decorator, when someone visits this 
+#this next line is executed
+def root():
+    return {"Hello": "World"}
