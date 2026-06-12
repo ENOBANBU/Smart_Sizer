35d4dafe502ea94a317f30bfb618268f7422684b
Eniola Bamgbelu <gbemds66@gmail.com>
2026-06-11 22:24:57 -0500

    added return statement to return important values and status message.

--- api.py
+++ api.py
@@ -32,6 +32,15 @@ async def scan_object(file: UploadFile = File(...), pixel_cm: float = 10):
         img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
         temp_path = "temp_load.png"
         cv2.imwrite(temp_path, img)
+        contour = jarvis_pipeline(temp_path, pixel_cm)
+        vol = rotate_solid(contour, pixel_cm)
+
+        return{
+            "status": "good",
+            "vol": round(vol, 2),
+            "contour_pts": len(contour),
+            "pixel_cm": pixel_cm
+        }
 
     except Exception as e:
         raise HTTPException(status_code=500, detail =str(e))
