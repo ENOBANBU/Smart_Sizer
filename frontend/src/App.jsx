a078a629e57bc04f9647a6d42cd70e001cfca137
Eniola Bamgbelu <gbemds66@gmail.com>
2026-06-16 21:27:50 -0500

    created react hook, app() component to build foundation for app, and exported app to be imported

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return(
+  <div className="app">
+    <h1>Smart-Sizer</h1>
+    <p> Uplaod an image to start volume estimation</p>
+    </div>
+  );
+}
+
+export default App;
