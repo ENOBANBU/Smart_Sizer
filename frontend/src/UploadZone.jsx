d72e02895dec2d294f543fe626ecb2b22e3b4d72
Eniola Bamgbelu <gbemds66@gmail.com>
2026-06-16 21:51:28 -0500

    fixed errors

--- frontend/src/UploadZone.jsx
+++ frontend/src/UploadZone.jsx
@@ -10,7 +10,7 @@ function UploadZone({onImageSelected}) {
     }, [onImageSelected]);
     const { getRootProps, getInputProps, isDragActive} = useDropzone({ onDrop, accept:{ 'image/png': [],}, multiple: false,});
     return (
-        <div {...getRootProps()} className = {'uploas-zone ${isDragActive ? 'active' : ''}'}>
+        <div {...getRootProps()} className={`upload-zone ${isDragActive ? 'active' : ''}`}>
             <input {...getInputProps()} />
             {isDragActive ? (<p>Drop the image here...</p> ):( <p>Drag and drop an image here, or click to select one</p>)}
             </div>
