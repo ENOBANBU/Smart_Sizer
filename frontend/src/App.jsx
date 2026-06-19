a9328c7858aba8973b5c6d0a1044cecc19c06184
Eniola Bamgbelu <gbemds66@gmail.com>
2026-06-19 01:48:52 -0500

    added handling image function to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState(null);
+
+  function handleImageUpload(file){
+    setImage(file);
+    console.log('Image uploaded:', file.name);
+  }
+  
   return(
   <div className="app">
     <h1>Smart-Sizer</h1>
